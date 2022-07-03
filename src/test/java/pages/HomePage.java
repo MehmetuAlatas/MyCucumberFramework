141f9c0e59ab34acc26f352d08dbee61ca1b61a3
Mehmet Alatas <mehmetualatas@gmail.com>
2022-07-03 08:47:04 +0300

    u

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -7,6 +7,7 @@ import utilities.Driver;
 
 public class HomePage {
 
+    //select[@name='car']
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
@@ -14,4 +15,3 @@ public class HomePage {
     public WebElement homeLoginButton;
 
 }
-//select[@name='car']
